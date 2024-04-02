3ba3b3e34f481400715b2c21628a0d4d64af5479
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2024-04-02 20:38:53 +0200

    fix: Fix gaplint for gd-files

--- lib/smallantimagmas.gd
+++ lib/smallantimagmas.gd
@@ -3,7 +3,6 @@
 #!  count number of antiassociative magmas of specified size <A>n</A> (a number) 
 DeclareGlobalFunction( "NrSmallAntimagmas" );
 
-
 #! @Arguments [n]
 #! @Description
 #!  count number of antiassociative magmas of specified size <A>n</A> (a number) 
